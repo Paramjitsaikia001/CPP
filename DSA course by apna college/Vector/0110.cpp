70e9832bc7b27fdcd06df7857d8ecdf86a14e4cc
Paramjitsaikia001 <paramjitsaikia@gmail.com>
2025-02-07 10:10:17 +0530

    update

--- DSA course by apna college/Vector/0110.cpp
+++ DSA course by apna college/Vector/0110.cpp
@@ -29,23 +29,80 @@
 //    vector<int> nums ={1,2,2,1,1};
 //    vector<int> ans = MJ(nums);
 //    cout<<ans[0]<<endl;
-//    return 0; 
+//    return 0;
 // }
+
 
 
 
 /*optimize */
 /*using sorting*/
+// #include <iostream>
+// #include <vector>
+// #include <algorithm>
+// using namespace std;
+// class solution
+// {
+// public:
+//    int MJ(vector<int> nums)
+//    {
+//       int n = nums.size();
+//       sort(nums.begin(), nums.end());
+//       int freq = 1, ans = nums[0];
+//       for (int i = 0; i < n; i++)
+//       {
+//          if (nums[i] == nums[i + 1])
+//          {
+//             freq++;
+//          }
+//          else{
+//            freq =1;
+//            ans =nums[i]; 
+//          } 
+//       if(freq>n/2){
+//          return ans;
+//       }
+//       }
+//       return ans;
+//    }
+// };
+// int main()
+// {
+//    solution sol;
+//    vector<int> nums = {1, 2, 2, 1, 1, 1};
+//    cout<<sol.MJ(nums);
+//    return 0;
+// }
+
+
+
+
+/*moore's voting algorithum*/
+
 #include<iostream>
 #include<vector>
 using namespace std;
 class solution{
    public:
    int MJ(vector<int> nums){
-      int n =nums.size();
-      sort(nums.begin(),nums.end());
-   }
-}
-int main(){
+      int frq=0,ans=nums[0];
+      for(int i =0;i<nums.size();i++){
+         if(frq == 0){
+            ans=nums[i];
+         }
+         if(ans == nums[i]){
+            frq ++;
+      }else {
+         frq--;
+      }
 
+      }
+      return ans;
+   }
+};
+int main(){
+solution sol;
+vector<int> nums ={1,2,2,1,1};
+cout<< sol.MJ(nums)<<endl;
+return 0;
 }
